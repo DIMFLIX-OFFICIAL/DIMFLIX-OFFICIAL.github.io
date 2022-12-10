2d6253136c35d6c6c4259a3dc09967ebe82ab734
DIMFLIX <112165977+DIMFLIX-OFFICIAL@users.noreply.github.com>
2022-12-10 17:51:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,24 +7,10 @@ tg.MainButton.color = document.documentElement.style.getPropertyValue('--tg-them
 tg.MainButton.show();
 tg.MainButton.setText("Отправить данные");
 
-let name = document.getElementById('name').value;
-let surname = document.getElementById('surname').value;
-let age = document.getElementById('age').value;
-let comment = document.getElementById('comment').value;
-
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    try {
-        
-        let name = document.getElementById('name').value;
-        tg.sendData(name);
-    } catch(e) {
-        let p = document.createElement("p");
-        p.innerText = e;
-        
-        let container = document.getElementById('container')
-        container.appendChild(p); 
-    }
+    let name = document.getElementById('name').value;
+    tg.sendData(name);
 });
 
 
