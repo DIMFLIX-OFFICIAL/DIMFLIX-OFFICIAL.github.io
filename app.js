e469c07c966bd91dfc087230232c001f5b906877
DIMFLIX <112165977+DIMFLIX-OFFICIAL@users.noreply.github.com>
2022-12-10 19:34:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+p.innerText = `Приветствую вас, ${tg.initDataUnsafe.user.first_name}`;
 
 usercard.appendChild(p); 
 
